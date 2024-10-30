d9b2f37da5ac55818652a555c2d0c2bfa885d3d5
Zdravko Galchov <zdravko.galchov@gmail.com>
2024-10-30 11:26:59 +0000

    Updated email sending form

--- sections/views.py
+++ sections/views.py
@@ -1,42 +1,51 @@
 from django.conf import settings
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 
 from .models import Skill
+from .forms import ContactForm
 
 
 def home(request):
+    context = {}
+    form = ContactForm(request.POST or None)
+    email_sent = False
+    sender_name = ''
+
     if request.method == 'POST':
+        if form.is_valid():
 
-        # Get form data
-        name = request.POST['name']
-        email = request.POST['email']
-        message = request.POST['message']
+            # Get form data
+            sender_name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
 
-        # Compose email
-        subject = f'New contact form submission from {name}'
-        email_message = f'Sender: {name}\nEmail: {email}\nMessage: {message}'
-        recipient_list = [settings.DEFAULT_FROM_EMAIL]
+            # Compose email
+            subject = f'New contact form submission from {sender_name}'
+            email_message = f'Sender: {sender_name}\nEmail: {email}\nMessage: {message}'
+            recipient_list = [settings.DEFAULT_FROM_EMAIL]
 
-        # Send email
-        send_mail(
-            subject=subject,
-            message=email_message,
-            from_email=email,
-            recipient_list=recipient_list,
-            fail_silently=False,
-        )
+            # Send email
+            send_mail(
+                subject=subject,
+                message=email_message,
+                from_email=email,
+                recipient_list=recipient_list,
+                fail_silently=False,
+            )
 
-        # Go to page after success
-        return render(request, 'home.html')
+            email_sent = True
+            form = ContactForm()
 
     key_skills = Skill.objects.filter(is_key_skill=True)
     tech_skills = Skill.objects.filter(is_key_skill=False)
 
-    context = {
+    context.update({
+        'form': form,
+        'sender_name': sender_name,
+        'email_sent': email_sent,
         'key_skills': key_skills,
         'tech_skills': tech_skills,
-    }
+    })
 
     return render(request, 'home.html', context)
