96fad448fb0acd82a80ef13d1bfbb40e81668465
Zdravko Galchov <zdravko.galchov@gmail.com>
2024-11-26 13:10:04 +0000

    Fixed the 'Hello' section appearance

--- sections/static/js/hello.js
+++ sections/static/js/hello.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const textElement = document.getElementById("typewriter-text");
-    const text = textElement.innerHTML;
-    textElement.innerHTML = "";
+    const text = textElement.textContent;
+    textElement.textContent = "";
 
     let index = 0;
     function typeWriter() {
         if (index < text.length) {
             textElement.innerHTML += text.charAt(index);
             index++;
-            setTimeout(typeWriter, 50);
+            setTimeout(typeWriter, 30);
         }
     }
 
